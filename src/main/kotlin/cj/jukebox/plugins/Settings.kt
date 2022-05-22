d39f19d39c9fe62fdd9b309f83a3bf17a6b13950
Ukabi <ukabi.sama@gmail.com>
2022-05-22 23:46:40 +0200

    Settings Progress - now handling form submission in settings page

--- src/main/kotlin/cj/jukebox/plugins/Settings.kt
+++ src/main/kotlin/cj/jukebox/plugins/Settings.kt
@@ -1,20 +1,30 @@
 package cj.jukebox.plugins
 
 import cj.jukebox.templates.Settings
+import cj.jukebox.utils.getUserSession
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.html.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.settings() {
     routing {
-        route("/settings") {
-            get {
-                call.respondHtmlTemplate(Settings("Test")) {}
-            }
-            post {
-                println("post settings")
-                call.respondRedirect("settings")
+        authenticate("auth-session") {
+            route("/settings") {
+                get {
+                    call.respondHtmlTemplate(Settings(call.getUserSession()!!.user.name)) {}
+                }
+                post {
+                    val parameters = call.receiveParameters()
+                    val style = parameters["style"]
+
+                    val session = call.getUserSession()!!
+                    session.user.theme = style
+
+                    call.respondRedirect("settings")
+                }
             }
         }
     }
