8637d9b980509f532aa0dbcde08188dce3b8b4b9
Khâtharsis <66702627+khatharsis42@users.noreply.github.com>
2022-05-22 04:21:56 +0200

    Javadoc et Flash pour Auth.kt

--- src/main/kotlin/cj/jukebox/templates/Auth.kt
+++ src/main/kotlin/cj/jukebox/templates/Auth.kt
@@ -3,12 +3,31 @@ package templates
 import cj.jukebox.config
 import cj.jukebox.templates.Header
 import io.ktor.server.html.*
+import kotlinx.css.header
 import kotlinx.css.script
 import kotlinx.html.*
 
-class Auth : Template<HTML> {
+/**
+ * Classe de template pour l'authentification.
+ *
+ * @param correct Indique si l'authentification précèdente était correcte.
+ * Vaut true de base pour des raisons évidentes.
+ * Permet de faire spawner un texte en rouge indiquant à l'utilisateur que le login n'a pas réussi.
+ * TODO: remplacer ça par une enum dans le futur, pour traiter tous les problèmes possibles.
+ * @author khatharsis
+ */
+class Auth(private val correct: Boolean = true) : Template<HTML> {
+    /**
+     * Le header.
+     */
     private val header = TemplatePlaceholder<Header>()
-    private val createJS = { action: String ->
+
+    /**
+     * Fonction utilisée pour générer du JavaScript utilisé par les boutons.
+     * Créé une copie des inputs pour les envoyer vers l'URL donnée en paramètre.
+     * @param action L'URL vers laquelle faire la requête POST.
+     */
+    private fun createJS(action: String) =
         """username = document.getElementById("inputUser").cloneNode();
            password = document.getElementById("inputPassword").cloneNode();
            var form = document.createElement("form");
@@ -20,14 +39,13 @@ class Auth : Template<HTML> {
            document.body.appendChild(form);
            form.submit();
            """.trimIndent()
-    }
 
     override fun HTML.apply() {
         insert(Header(), header)
         body("text-center") {
             div("container") {
-                //TODO: trouver le moyen de faire des flashs.
                 h1("h3 mb-3 font-weight-normal") { text("C'est koi ton p'tit nom ?") }
+                if (!correct) h2 { style="color:red"; text("Mot de passe ou nom d'utilisateur incorrect.")}
                 label("sr-only") {
                     htmlFor = "inputUser"
                     text("Pseudo")
@@ -52,6 +70,7 @@ class Auth : Template<HTML> {
                     required = true
                     autoFocus = true
                 }
+                br
                 button {
                     classes = "btn btn-lg btn-primary btn-block".split(" ").toSet()
                     name = "action"
