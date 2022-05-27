69610107c7160bcd89f2203c7a71065c9c7e536e
Ukabi <ukabi.sama@gmail.com>
2022-05-27 03:59:27 +0200

    Upgrade on TrackStatistics - now able to show users with how many times track has been requested

--- src/main/kotlin/cj/jukebox/plugins/statistics/TrackStatistics.kt
+++ src/main/kotlin/cj/jukebox/plugins/statistics/TrackStatistics.kt
@@ -1,5 +1,6 @@
 package cj.jukebox.plugins.statistics
 
+import cj.jukebox.database.Log
 import cj.jukebox.database.Track
 import cj.jukebox.templates.MainTemplate
 import cj.jukebox.utils.UserSession
@@ -12,35 +13,39 @@ import kotlinx.html.*
  * @param[track] La musique dont on veut voir les stats.
  * @author khatharsis
  */
-class TrackStatistics(user: UserSession, track: Track) : MainTemplate(
-    user,
-    content = object : Template<FlowContent> {
-        private val statsColumn = TemplatePlaceholder<StatsColumn>()
-        override fun FlowContent.apply() {
-            div("container") {
-                div {
-                    style = "text-align:center"
-                    h1 { text("Statistiques de ${track.track}") }
-                    when {
-                        track.obsolete -> h2 { style = "color:red"; text("Obsolete track") }
-                        track.blacklisted -> h2 { style = "color:red"; text("Blacklisted track") }
-                        else -> form {
-                            action = "/add/${track.id}"
-                            method = FormMethod.post
-                            input {
-                                type = InputType.submit
-                                name = "Add"
-                                value = "Ajouter à la file d'attente."
-                            }
+class TrackStatistics(user: UserSession, track: Track) : MainTemplate(user, content = object : Template<FlowContent> {
+    private val statsColumn = TemplatePlaceholder<StatsColumn>()
+    override fun FlowContent.apply() {
+        div("container") {
+            div {
+                style = "text-align:center"
+                h1 { text("Statistiques de ${track.track}") }
+                when {
+                    track.obsolete -> h2 { style = "color:red"; text("Obsolete track") }
+                    track.blacklisted -> h2 { style = "color:red"; text("Blacklisted track") }
+                    else -> form {
+                        action = "/add/${track.id}"
+                        method = FormMethod.post
+                        input {
+                            type = InputType.submit
+                            name = "Add"
+                            value = "Ajouter à la file d'attente."
                         }
                     }
                 }
-                div("statistics") {
-                    div("col statcol") {
-                        insert(StatsColumn("All Time", giveTestArray()), statsColumn)
-                    }
+            }
+            div("statistics") {
+                div("col statcol") {
+                    insert(StatsColumn("All Time", prepareData(track)), statsColumn)
                 }
             }
         }
     }
-)
+})
+
+/**
+ * Prépare les données nécessaires à l'affichage des statistiques.
+ * @author Ukabi
+ */
+private fun prepareData(track: Track, timeDelta: Int? = null): List<List<Any>> =
+    listOf(listOf("User", "Count")) + Log.getMostActiveUsers(track).map { listOf(it.second.name, it.first) }
