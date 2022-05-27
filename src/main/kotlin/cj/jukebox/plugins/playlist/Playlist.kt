4e4331b0993ac2632b7ea25a17d667e516d1e205
Ukabi <ukabi.sama@gmail.com>
2022-05-28 00:40:43 +0200

    Switched Playlist Type Elements from Pair<Int, track> to Log - because it's much simpler like that, I made it uselessly overcomplicated

--- src/main/kotlin/cj/jukebox/plugins/playlist/Playlist.kt
+++ src/main/kotlin/cj/jukebox/plugins/playlist/Playlist.kt
@@ -3,7 +3,7 @@ package cj.jukebox.plugins.playlist
 import cj.jukebox.database.*
 import cj.jukebox.utils.UserSession
 
-typealias Playlist = MutableList<Pair<Int, Track>>
+typealias Playlist = MutableList<Log>
 
 /**
  * Vérifie si la [track] fournie peut être jouée, puis l'ajoute à la [Playlist].
@@ -12,7 +12,7 @@ typealias Playlist = MutableList<Pair<Int, Track>>
  * @author Ukabi
  */
 fun Playlist.addIfPossible(track: Track, user: User): Boolean =
-    !track.blacklisted && !track.obsolete && add(Pair(Log.createLog(track, user).time, track))
+    !track.blacklisted && !track.obsolete && add(Log.createLog(track, user))
 
 /**
  * Vérifie si la [Track] correspondant à la [trackId] fournie peut être jouée, puis l'ajoute à la [Playlist].
@@ -44,12 +44,12 @@ fun Playlist.addIfPossible(track: Track, user: UserSession): Boolean = addIfPoss
  * Efface aussi l'occurrence de [Log] précédemment créée si [delete] est fourni.
  * @author Ukabi
  */
-fun Playlist.removeIfPossible(track: Track, delete: Boolean = false): Boolean =
+fun Playlist.removeIfPossible(track: Track, delete: Boolean = true): Boolean =
     Log
         .getTrackLogs(track)
-        .firstOrNull { Pair(it.time, it.trackId) in this }
+        .firstOrNull { it in this }
         ?.let { if (delete) it.delete(); it }
-        .let { (it != null) && remove(Pair(it.time, it.trackId)) }
+        .let { (it != null) && remove(it) }
 
 /**
  * Vérifie si la [Track] correspondant à la [trackId] fournie fait partie de la [Playlist], puis la supprime.
@@ -66,18 +66,6 @@ fun Playlist.removeIfPossible(trackId: Int, delete: Boolean = false): Boolean =
 fun Playlist.move(from: Int, to: Int) {
     this[from] = this[to].also { this[to] = this[from] }
 }
-
-/**
- * Renvoie une [List] de [n] des précédents [Log] dont la [Track] peut être jouée.
- * TODO: probablement à refactor vers database.Logs pour en faire une fonction moins coûteuse en mémoire.
- * @author Ukabi
- */
-fun Playlist.suggest(n: Int = 5): List<Log> =
-    Log
-        .getLogs()
-        .filter { !it.trackId.blacklisted && !it.trackId.obsolete }
-        .shuffled()
-        .slice(0 until n)
 
 /**
  * Renvoie la somme des durées (en secondes) de chacune des [Track] de la [Playlist].
