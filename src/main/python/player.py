fc04a769d4da404ac1fcfc4da90ed0c60fcbc202
Ukabi <ukabi.sama@gmail.com>
2022-05-28 09:46:15 +0200

    Starting Implementation of Player - wtf Python? - TODO: make Kotlin side (start of script + sending signals)

---
+++ src/main/python/player.py
@@ -0,0 +1,96 @@
+import mpv
+import requests
+import sys
+import signal
+import itertools
+
+from typing import (
+    Iterable,
+    List,
+    NamedTuple
+)
+
+
+def get_player() -> mpv.MPV:
+    """Returns a new instance of MPV player, in case none exists or old one was broken."""
+    return mpv.MPV(ytdl=True)
+
+
+# MPV player
+player: mpv.MPV = get_player()
+
+
+class SyncData(NamedTuple):
+    """Representation of data received from server."""
+    playlist: List[str]
+    ids: List[int]
+
+
+def _soft_interrupt(sig_num, stack_frame):
+    """On signal, asking script to make player end it's current process."""
+    player.stop()
+
+
+def _next(sig_num, stack_frame):
+    """On signal, asking script to skip current song."""
+    global to_skip
+    to_skip = True
+
+
+def _update(sig_num, stack_frame):
+    """On signal, asking script to ask for a playlist update."""
+    global to_update
+    to_update = True
+
+
+def update():
+    """Requests server the new state of playlist, and updates player appropriately."""
+    updated_playlist = SyncData(**requests.get(url).json()).playlist
+
+    player.playlist_clear()
+    for url_or_path in updated_playlist:
+        player.playlist_append(url_or_path)
+
+
+if __name__ == "__main__":
+    # Checking if request url was provided
+    if not (args := sys.argv[1:]):
+        raise ValueError("Host name required.")
+
+    # Global variables for script
+    to_skip = False
+    to_update = True
+    url = args[0]
+
+    # Registering end and update signals
+    signal.signal(signal.SIGINT, _soft_interrupt)  # CTRL+C
+    signal.signal(signal.SIGUSR1, _next)          # Custom signal
+    signal.signal(signal.SIGUSR2, _update)        # Custom signal
+
+    while True:
+        try:
+            # Waiting for track to end, or skipping
+            # it if skip signal was received
+            if not to_skip:
+                player.wait_for_playback()
+                to_skip = False
+
+            # Removing the track that just ended playing
+            player.playlist_remove(0)
+
+            # If update signal was received, resets playlist
+            # by requesting new playlist state
+            if to_update:
+                update()
+                to_update = False
+
+            # After all checkups, processing to play next track
+            player.playlist_next()
+
+            # Sending back the current state of player to server
+            requests.post(url, ids)
+
+        # An error that happens sometimes, just
+        # ignoring it and restarting player
+        except mpv.ShutdownError as e:
+            player: mpv.MPV = get_player()
