95c674abcc9dcb36bfa674c74ec35bdd36f0a508
Al3xCalibur <alexandre.conte11@gmail.com>
2022-05-29 22:14:49 +0200

    Align kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ val logbackVersion: String by project
 plugins {
     application
 
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.6.21"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.6.21"
 }
 
