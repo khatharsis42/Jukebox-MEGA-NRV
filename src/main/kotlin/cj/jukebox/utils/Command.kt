3bedfb98346b8abe39bd55d0d6ec631f104a7009
Hadrien GUELQUE <hadrien.guelque@telecom-sudparis.eu>
2022-05-31 11:51:23 +0200

    Ajout de commandes.

--- src/main/kotlin/cj/jukebox/utils/Command.kt
+++ src/main/kotlin/cj/jukebox/utils/Command.kt
@@ -5,15 +5,26 @@ import java.util.concurrent.TimeUnit
 
 enum class SigName { SIGINT, SIGUSR1, SIGUSR2 }
 
-fun String.runCommand(workingDir: File = File("."), toNbr: Long = 60, toUnit: TimeUnit = TimeUnit.SECONDS): Process? =
+/**
+ * Lance la String sous la forme d'une commande. Opère un split(" ").
+ * @author Khâtharsis
+ */
+@Deprecated("This is unsafe, you should use it on a List<String> instead.")
+fun String.runCommand(workingDir: File = File("."), toNbr: Long = 60, toUnit: TimeUnit = TimeUnit.SECONDS) = Regex("\\s").split(this).runCommand(workingDir, toNbr, toUnit)
+
+/**
+ * Lance une commande à partir d'une liste de String.
+ * Chaque item de la liste est lu tel quel.
+ * @author Khâtharsis
+ */
+fun List<String>.runCommand(workingDir: File = File("."), toNbr: Long = 60, toUnit: TimeUnit = TimeUnit.SECONDS): Process? =
     runCatching {
-        ProcessBuilder(Regex("\\s").split(this))
+        ProcessBuilder(this)
             .directory(workingDir)
-            .redirectOutput(ProcessBuilder.Redirect.PIPE)
-            .redirectError(ProcessBuilder.Redirect.PIPE)
+            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+            .redirectError(ProcessBuilder.Redirect.INHERIT)
             .start().also { it.waitFor(toNbr, toUnit) }
     }.onFailure { it.printStackTrace() }.getOrNull()
-
 fun Process.sendSignal(sigName: SigName) {
     "kill -${sigName.name} ${pid().toInt()}".runCommand()
 }
